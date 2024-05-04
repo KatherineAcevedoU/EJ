b2a44637eebb9e9fdbb7c83192ee7df51ebde9d8
Katherine Acevedo <katherineacecedo543@gmail.com>
2024-05-04 00:16:01 -0600

    Se creo el proyecto

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string saludo = "";
+
+            Console.WriteLine("Hola, soy la primera version");
+            Console.WriteLine("Ingrese un saludo");
+            saludo = Console.ReadLine();
+
+
+            Console.WriteLine("El saludo es. " + saludo);
+
+
+        }
+    }
+}
